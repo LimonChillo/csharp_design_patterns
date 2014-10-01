3b60415849ce194030a6d48215c30999d652b366
Simon Hintersonnleinter <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2014-10-01 17:24:11 +0200

    add a managerclass

---
+++ bsp01/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace bsp01
+{
+	public class Manager
+	{
+		public Manager ()
+		{
+		}
+
+
+		List<Duck> allDucks = new List<Duck>();
+
+		public void addDuck(Duck d, Quack q)
+		{
+			allDucks.Add(d);
+		}
+	}
+}
+
