d7b87c29ea537183aedc495bbcb0dacde42fe28f
Simon Hintersonnleinter <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2014-10-01 19:33:33 +0200

    add new projekt

---
+++ bsp02/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace bsp02
+{
+	class Account
+	{
+
+		private string name;
+		private double money = 0;
+
+		public Account (string name)
+		{
+			this.name = name;
+		}
+			
+		public void deposit(double d)
+		{
+			money += d;
+		}
+
+		public void withDraw(double d)
+		{
+			money -= d;
+		}
+		public double getBalance()
+		{
+			return money;
+		}
+		public string getName()
+		{
+			return name;
+		}
+
+
+	}
+}
+
---
+++ bsp02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bsp02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Manager m = Manager.getInstance ();
+
+			m.addAccount (new Account ("Herbert"));
+			m.addAccount (new Account ("Anton"));
+			m.addAccount (new Account ("Berta"));
+			m.addAccount (new Account ("Cäser"));
+
+			m.printAllAccounts ();
+
+		}
+	}
+}
