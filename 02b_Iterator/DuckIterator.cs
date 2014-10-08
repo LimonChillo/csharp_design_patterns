0177c65b5808983c4deff5d87644a6ab53ab07b0
David Kranewitter <dkranewitter.mmt-b2013@fh-salzburg.ac.at>
2014-10-08 20:20:24 +0200

    added DuckIterator

---
+++ 02b_Iterator/DuckIterator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b_Iterator
+{
+	public class DuckIterator : IIterator
+	{
+		private List<Duck> duckList;
+		private long current;
+
+		public DuckIterator ()
+		{
+
+		}
+
+		public void first()
+		{
+			current = 0;
+		}
+
+		public void next()
+		{
+			current++;
+		}
+
+		public bool isDone()
+		{
+			return current >= duckList.Count();
+		}
+
+		public Duck currentItem()
+		{
+			if (isDone ()) {
+				trow IteratorOutOfBounds;
+			}
+			return duckList [current];
+		}
+	}
+}
+
