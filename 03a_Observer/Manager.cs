d3551c908d2523d05fb5f621c442a35908eb2767
Simon Hintersonnleinter <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2014-10-20 15:20:20 +0200

    initial commit of 03a_Observer

---
+++ 03a_Observer/Manager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace bsp02
+{
+	class Manager
+	{
+		private static Manager instance;
+
+		private List<Account> allAccounts = new List<Account> ();
+
+		private Manager ()
+		{
+
+		}
+
+
+		public static Manager getInstance()
+		{
+			if (instance == null) 
+			{
+				instance = new Manager ();
+			}
+			return instance;
+		}
+
+		public void addAccount(Account a)
+		{
+			allAccounts.Add (a);
+		}
+
+		public void removeAccount(Account a)
+		{
+			allAccounts.Remove (a);
+		}
+		public void printAllAccounts()
+		{
+			foreach (Account a in allAccounts)
+				Console.WriteLine (a.getName () + " " + a.getBalance ());
+		}
+		public void desposit(double d)
+		{
+
+		}
+		
+
+	}
+}
+
