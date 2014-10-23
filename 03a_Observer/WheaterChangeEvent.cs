86a77f117ed9846e1511b12b2756e7eccca0d238
Simon Hintersonnleinter <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2014-10-23 18:04:51 +0200

    add WheaterChangeEvent

---
+++ 03a_Observer/WheaterChangeEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace a_Observer
+{
+	public class WheaterChangeEvent:EventArgs
+	{
+
+		float humidty;
+		float temperture;
+		float pressure;
+
+		public WheaterChangeEvent (float humidty,float temperture,float pressure)
+		{
+			this.humidty = humidty;
+			this.temperture = temperture;
+			this.pressure = pressure;
+		}
+	}
+}
+
