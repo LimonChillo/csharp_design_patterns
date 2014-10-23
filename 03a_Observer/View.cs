c5402f2976289e1deb579048091a441da08c8cc8
Simon Hintersonnleinter <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2014-10-23 18:00:17 +0200

    rename view.cs

---
+++ 03a_Observer/View.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bsp03
+{
+	class View:IObserver
+	{
+
+		private string name; 
+
+		public View (string name)
+		{
+			this.name = name;
+		}
+
+		public void update()
+		{
+
+		}
+	}
+}
+
