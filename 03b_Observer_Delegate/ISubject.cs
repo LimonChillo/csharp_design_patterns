ca421db54ff8ac331adf579b6d404a502cc55494
David Kranewitter <dkranewitter.mmt-b2013@fh-salzburg.ac.at>
2014-10-27 18:05:27 +0100

    bugfix weatherstation

---
+++ 03b_Observer_Delegate/ISubject.cs
@@ -0,0 +1,17 @@
+﻿/*
+	David Kranewitter
+	Simon Hintersonnleitner
+*/
+
+using System;
+
+namespace bsp03b
+{
+	interface ISubject
+	{
+		void registerObserver (View v);
+		bool removeObserver (View v);
+		void notifyObservers ();
+	}
+}
+
