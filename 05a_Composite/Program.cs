bec53ccfff986526131cda5ff7e11f6e6512692f
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2014-11-26 11:43:00 +0100

    * Exercises.sln: * Exercises.userprefs: * Program.cs: * 05a_Composite.csproj: * AssemblyInfo.cs:
    * 04a_Decorator.csproj.FilesWrittenAbsolute.txt: add 05_Composite

---
+++ 05a_Composite/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ 	David Kranewitter
+ 	Simon Hintersonnleitner
+*/
+
+using System;
+
+namespace a_Composite
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
